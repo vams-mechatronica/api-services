9bbe3ba669c367df0077568f3c1eb1e3daf3250b
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2025-10-23 00:30:15 +0530

    Fix

--- src/notifications/management/commands/run_email_campaign.py
+++ src/notifications/management/commands/run_email_campaign.py
@@ -29,7 +29,7 @@ class Command(BaseCommand):
                 offset = 0
 
         contacts = MarketingContact.objects.filter(active=True).exclude(email__isnull=True)[offset:limit+offset]
-        self.stdout.write(self.style.HTTP_INFO("total contacts found: %s", len(contacts)))
+        self.stdout.write(self.style.HTTP_INFO(f"total contacts found:{len(contacts)}"))
         sent_count = 0
         for contact in contacts[offset:offset + limit]:
             log, created = EmailMarketingLog.objects.get_or_create(
